9115f0f8b537345bb52060eefa018c938b272fa6
asmku234c2s14853 <nagalakshmikirushnan@gmail.com>
2025-07-03 16:37:13 +0530

    Add Media Queries

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
 /*=============== SHOW MENU ===============*/
 
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+
+
 /*===== Menu Show =====*/
 /* Validate if constant exists */
 
+if(navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+
 /*===== Hide Show =====*/
 /* Validate if constant exists */
+
+if(navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
 
 /*=============== IMAGE GALLERY ===============*/
 
@@ -31,13 +49,21 @@ var swiperCategories = new Swiper('.categories__container', {
     prevEl: '.swiper-button-prev',
   },
   breakpoints: {
-        640: {
+        350: {
           slidesPerView: 2,
-          spaceBetween: 20,
+          spaceBetween: 24,
         },
         768: {
+          slidesPerView: 3,
+          spaceBetween: 24,
+        },
+        992: {
           slidesPerView: 4,
-          spaceBetween: 40,
+          spaceBetween: 24,
+        },
+        1200: {
+          slidesPerView: 5,
+          spaceBetween: 24,
         },
         1400: {
           slidesPerView: 6,
@@ -56,13 +82,13 @@ var swiperProducts = new Swiper('.new__container', {
     prevEl: '.swiper-button-prev',
   },
   breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
         768: {
-          slidesPerView: 4,
-          spaceBetween: 40,
+          slidesPerView: 2,
+          spaceBetween: 24,
+        },
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 24,
         },
         1400: {
           slidesPerView: 4,
